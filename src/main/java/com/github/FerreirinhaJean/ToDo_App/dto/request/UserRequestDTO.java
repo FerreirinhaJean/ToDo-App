cc2112ffe4c28b896418a24401cb13599b41d77c
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-15 11:53:12 -0300

    feat: add validations to password field DTO

--- src/main/java/com/github/FerreirinhaJean/ToDo_App/dto/request/UserRequestDTO.java
+++ src/main/java/com/github/FerreirinhaJean/ToDo_App/dto/request/UserRequestDTO.java
@@ -2,6 +2,8 @@ package com.github.FerreirinhaJean.ToDo_App.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record UserRequestDTO(
 
@@ -13,6 +15,11 @@ public record UserRequestDTO(
         String email,
 
         @NotBlank(message = "The password is required.")
+        @Size(min = 8, max = 32, message = "Password length must be between 8 and 32 characters.")
+        @Pattern(
+                regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^A-Za-z0-9]).+$",
+                message = "Password does not meet the required security criteria. It must contain at least one lowercase letter, one uppercase letter, one number, and one special character."
+        )
         String password
 ) {
 }
