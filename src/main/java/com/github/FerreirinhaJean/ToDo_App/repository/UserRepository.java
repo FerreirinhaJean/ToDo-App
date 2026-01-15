709413d240696dfbada36ac0bc8149bdafe1b20e
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-15 09:52:13 -0300

    feat: create UserRepository

---
+++ src/main/java/com/github/FerreirinhaJean/ToDo_App/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.github.FerreirinhaJean.ToDo_App.repository;
+
+import com.github.FerreirinhaJean.ToDo_App.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
