d50435b317dc05a1b6bfc903e3cc2274846f48e8
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-15 11:19:59 -0300

    feat: create user service and repository

--- src/main/java/com/github/FerreirinhaJean/ToDo_App/service/UserService.java
+++ src/main/java/com/github/FerreirinhaJean/ToDo_App/service/UserService.java
@@ -1,7 +1,11 @@
 package com.github.FerreirinhaJean.ToDo_App.service;
 
+import com.github.FerreirinhaJean.ToDo_App.dto.request.UserRequestDTO;
+import com.github.FerreirinhaJean.ToDo_App.entity.User;
+import com.github.FerreirinhaJean.ToDo_App.exception.DuplicatedRegisterException;
 import com.github.FerreirinhaJean.ToDo_App.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +13,17 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public User create(UserRequestDTO userRequestDTO) throws DuplicatedRegisterException {
+        if (userRepository.existsByEmail(userRequestDTO.email()))
+            throw new DuplicatedRegisterException("Email already is used.");
+
+        User user = new User();
+        user.setName(userRequestDTO.name());
+        user.setEmail(userRequestDTO.email());
+        user.setPassword(passwordEncoder.encode(userRequestDTO.password()));
+
+        return userRepository.save(user);
+    }
 }
