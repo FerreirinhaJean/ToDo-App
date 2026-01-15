9d0bb9f957d2c0648f586320b62811beef2817d6
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-15 09:47:28 -0300

    feat: create entity User

---
+++ src/main/java/com/github/FerreirinhaJean/ToDo_App/entity/User.java
@@ -0,0 +1,39 @@
+package com.github.FerreirinhaJean.ToDo_App.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Instant updatedAt;
+
+}
