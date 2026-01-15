7f75026450419cfe7c60f7151de2557a396fe1c3
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-15 11:20:16 -0300

    feat: create Auth controller to register new users

---
+++ src/main/java/com/github/FerreirinhaJean/ToDo_App/controller/AuthController.java
@@ -0,0 +1,47 @@
+package com.github.FerreirinhaJean.ToDo_App.controller;
+
+import com.github.FerreirinhaJean.ToDo_App.dto.request.UserRequestDTO;
+import com.github.FerreirinhaJean.ToDo_App.dto.response.UserResponseDTO;
+import com.github.FerreirinhaJean.ToDo_App.entity.User;
+import com.github.FerreirinhaJean.ToDo_App.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(
+            @RequestBody @Valid UserRequestDTO userRequestDTO
+    ) {
+        User user = userService.create(userRequestDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        UserResponseDTO responseDTO = new UserResponseDTO(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getCreatedAt()
+        );
+
+        return ResponseEntity.created(location).body(responseDTO);
+    }
+
+}
