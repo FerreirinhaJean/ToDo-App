01fb6fede528c16b384d2437cb3108f4ea7eb14c
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-16 09:49:15 -0300

    feat: configurer SecurityFilterChain to Authorization Server and create Bean to RegisteredClientsRepository from database

--- src/main/java/com/github/FerreirinhaJean/ToDo_App/config/SecurityConfiguration.java
+++ src/main/java/com/github/FerreirinhaJean/ToDo_App/config/SecurityConfiguration.java
@@ -2,19 +2,45 @@ package com.github.FerreirinhaJean.ToDo_App.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.client.JdbcRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
     @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = OAuth2AuthorizationServerConfigurer.authorizationServer();
+
+        httpSecurity.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, (oAuth2AuthorizationServerConfigurer ->
+                        oAuth2AuthorizationServerConfigurer.oidc(Customizer.withDefaults())))
+                .authorizeHttpRequests(authorizer -> {
+                    authorizer.anyRequest().authenticated();
+                }).exceptionHandling(
+                        exceptionHandling ->
+                                exceptionHandling.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login")));
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    @Order(Ordered.LOWEST_PRECEDENCE)
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
@@ -23,6 +49,7 @@ public class SecurityConfiguration {
                     authorizer.anyRequest().authenticated();
                 })
                 .formLogin(form -> form.loginPage("/login").permitAll())
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()))
                 .build();
     }
 
@@ -30,5 +57,12 @@ public class SecurityConfiguration {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(15);
     }
+
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(JdbcTemplate jdbcTemplate) {
+        return new JdbcRegisteredClientRepository(jdbcTemplate);
+    }
+
 
 }
