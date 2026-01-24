e224f248361221ad9e68f53a706ac426c0084244
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-24 15:20:51 -0300

    test: create tests to UserRepository and init UserServiceTest

--- src/test/java/com/github/FerreirinhaJean/ToDo_App/ToDoAppApplicationTests.java
+++ src/test/java/com/github/FerreirinhaJean/ToDo_App/ToDoAppApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.FerreirinhaJean.ToDo_App;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ToDoAppApplicationTests {
 
 	@Test
---
+++ src/test/java/com/github/FerreirinhaJean/ToDo_App/service/UserServiceTest.java
@@ -0,0 +1,10 @@
+package com.github.FerreirinhaJean.ToDo_App.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+}
