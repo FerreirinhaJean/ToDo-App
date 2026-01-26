144e3ef746325cb8e564a638e7d71765d728c560
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2026-01-26 10:21:36 -0300

    test: create and adjust sql to tests

--- src/test/resources/sql/insert_users.sql
+++ src/test/resources/sql/insert_users.sql
@@ -10,3 +10,16 @@ VALUES ('4d770cb8-67cf-498d-901e-03c541bb38f6',
         '$2a$15$v.PshoGNJmxNMljhcBSzk.F1mBJsNe.QXaRsmh6aaY1iJ8FRN8b4C',
         current_timestamp,
         current_timestamp);
+
+INSERT INTO users (id,
+                   name,
+                   email,
+                   password,
+                   created_at,
+                   updated_at)
+VALUES ('2983de2a-f98b-40bb-9adf-67cb4bdbc471',
+        'Tony Stark',
+        'iron.man@gmail.com',
+        '$2a$15$v.PshoGNJmxNMljhcBSzk.F1mBJsNe.QXaRsmh6aaY1iJ8FRN8b4C',
+        current_timestamp,
+        current_timestamp);
